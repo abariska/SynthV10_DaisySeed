82f77a30f553125ccc20ea55495ce9767afcf959
BorysKuzmenko <abariska@gmail.com>
2025-07-07 00:08:31 +0200

    sx_pins_changed

--- SX1509_extender.cpp
+++ SX1509_extender.cpp
@@ -11,8 +11,8 @@ void InitSX1509Buttons() {
     i2c_conf_buttons.transport_config.i2c_config.periph         = I2CHandle::Config::Peripheral::I2C_1;
     i2c_conf_buttons.transport_config.i2c_config.mode           = I2CHandle::Config::Mode::I2C_MASTER;
     i2c_conf_buttons.transport_config.i2c_config.speed          = I2CHandle::Config::Speed::I2C_400KHZ;
-    i2c_conf_buttons.transport_config.i2c_config.pin_config.scl = seed::D13;
-    i2c_conf_buttons.transport_config.i2c_config.pin_config.sda = seed::D14;
+    i2c_conf_buttons.transport_config.i2c_config.pin_config.scl = seed::D11;
+    i2c_conf_buttons.transport_config.i2c_config.pin_config.sda = seed::D12;
     i2c_conf_buttons.transport_config.i2c_address = 0x3E;
     sx1509_buttons.Init(i2c_conf_buttons);
     sx1509_buttons.Check();
@@ -29,8 +29,8 @@ void InitSX1509Encoders() {
     i2c_conf_encoders.transport_config.i2c_config.periph         = I2CHandle::Config::Peripheral::I2C_1;
     i2c_conf_encoders.transport_config.i2c_config.mode           = I2CHandle::Config::Mode::I2C_MASTER;
     i2c_conf_encoders.transport_config.i2c_config.speed          = I2CHandle::Config::Speed::I2C_400KHZ;
-    i2c_conf_encoders.transport_config.i2c_config.pin_config.scl = seed::D13;
-    i2c_conf_encoders.transport_config.i2c_config.pin_config.sda = seed::D14;
+    i2c_conf_encoders.transport_config.i2c_config.pin_config.scl = seed::D11;
+    i2c_conf_encoders.transport_config.i2c_config.pin_config.sda = seed::D12;
     i2c_conf_encoders.transport_config.i2c_address = 0x3F;
     sx1509_encoders.Init(i2c_conf_encoders);
     sx1509_encoders.Check();
@@ -48,8 +48,8 @@ void InitSX1509Leds() {
     i2c_conf_leds.transport_config.i2c_config.periph         = I2CHandle::Config::Peripheral::I2C_1;
     i2c_conf_leds.transport_config.i2c_config.mode           = I2CHandle::Config::Mode::I2C_MASTER;
     i2c_conf_leds.transport_config.i2c_config.speed          = I2CHandle::Config::Speed::I2C_400KHZ;
-    i2c_conf_leds.transport_config.i2c_config.pin_config.scl = seed::D13;
-    i2c_conf_leds.transport_config.i2c_config.pin_config.sda = seed::D14;
+    i2c_conf_leds.transport_config.i2c_config.pin_config.scl = seed::D11;
+    i2c_conf_leds.transport_config.i2c_config.pin_config.sda = seed::D12;
     i2c_conf_leds.transport_config.i2c_address = 0x70;
     sx1509_leds.Init(i2c_conf_leds);
     sx1509_leds.Check();
